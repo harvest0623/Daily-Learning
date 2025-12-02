d818469a784770774e4d3b9caa62f3c6242c8110
harvest0623 <3367741939@qq.com>
2025-12-02 21:25:23 +0800

    类型转换

---
+++ JS/类型转换/3.js
@@ -0,0 +1,12 @@
+// if(1 == '1'){
+//     console.log('hello');
+// }
+
+// const arr = {};
+// if(arr) {
+//     console.log('hello');
+// }
+
+// console.log({} == 1); // Number({})
+// console.log({} + '1'); // [object Object]1
+console.log([1, 2, 3] + '1');
