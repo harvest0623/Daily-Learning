4c441bfafb4c0acd75b2bf2c53dd0489234e6470
harvest0623 <3367741939@qq.com>
2026-03-04 16:14:35 +0800

    面试-JS系列

--- 面试考点/JS系列/1.js
+++ 面试考点/JS系列/1.js
@@ -14,5 +14,66 @@ const arr = [1, 2, 3, 4, 5];
 // });
 // console.log(arr, newArr);
 
-// console.log(arr.toString());
+// map 改：映射数组，返回新数组，不改变原数组
+// const newArr = arr.map((item, index, array) => {
+//   return item * 10
+// })
+// console.log(newArr);
+
+// fill 改：填充数组，返回新数组，不改变原数组
+// const newArr = new Array(5).fill(0);
+// console.log(newArr);
+
+// find 查：查找数组，返回第一个符合条件的元素，不改变原数组
+// const newArr = arr.find((item, index, array) => {
+//   return item === 6
+// })
+
+// findIndex 查：查找数组，返回第一个符合条件的元素的索引，不改变原数组
+// const newArr = arr.findIndex((item, index, array) => {
+//   return item === 6
+// })
+// console.log(newArr);
+
+// includes 查：判断数组是否包含某个元素，返回布尔值，不改变原数组
+// const index = arr.includes(2)
+// console.log(index);
+
+// toReversed 查：反转数组，返回新数组，不改变原数组
+// console.log(arr.toReversed().reverse());
+
+// sort 改：排序数组，返回新数组，不改变原数组
+// console.log(arr.sort((a, b) => {
+//   return b - a
+// }));
+
+// join 查：将数组元素连接成字符串，返回字符串，不改变原数组
 // console.log(arr.join(''));
+
+// const num = arr.reduce((pre, item, index, array) => {
+//   return pre + item
+// }, 0)
+
+// console.log(num);
+
+// reduce 查：累加数组元素，返回累加值，不改变原数组
+// const num = arr.reduce((pre, item, index, array) => {
+//   return pre + item
+// }, 0)
+// console.log(num);
+
+// every 查：判断数组是否所有元素都符合条件，返回布尔值，不改变原数组
+// const has = arr.every((item, index, array) => {
+//   return item > 0
+// })
+// console.log(has);
+
+// findLastIndex 查：查找数组，返回最后一个符合条件的元素的索引，不改变原数组
+// const has = arr.findLastIndex((item, index, array) => {
+//   return item === 1
+// })
+// console.log(has);
+
+// copyWithin 改：复制数组元素，返回新数组，不改变原数组
+const has = arr.copyWithin(0, 3)
+console.log(has);
---
+++ 面试考点/JS系列/2.js
@@ -0,0 +1,20 @@
+let str = ' Hello '
+
+// let newStr = str.concat('world');  // 合并字符串
+// let newStr = str.substring(1, 4);  // 截取字符串
+
+// let newStr = str.trimRight();  // 去除字符串右侧空格
+
+// let newStr = str.toLowerCase();   // 转换为小写字母
+
+// let newStr = str.padStart(9, 8);  // 左侧填充字符串
+// let newStr = str.padEnd(9, 8);  // 右侧填充字符串
+
+// let newStr = str.repeat(2);  // 重复字符串
+// let newStr = str.replaceAll('l', 'n');  // 替换所有子字符串
+
+// let newStr = str.endsWith(' H');  // 判断字符串是否以指定子字符串结尾
+
+// console.log(str.split(''));  // 将字符串转换为数组
+
+console.log(str.search(/H/));  // 查找字符串，返回第一个符合条件的元素的索引
