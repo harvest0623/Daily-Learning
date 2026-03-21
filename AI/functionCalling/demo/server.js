a739a80fe790c76e1e1c58f7b0c550bc33bb2051
harvest0623 <3367741939@qq.com>
2026-03-21 17:06:08 +0800

    functionCalling下

--- AI/functionCalling/demo/server.js
+++ AI/functionCalling/demo/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import axios from 'axios'
 import * as dotenv from 'dotenv'
+import { exec } from 'child_process'
 
 dotenv.config({  // 加载环境变量
     path: ['.env.local', '.env']
@@ -16,39 +17,67 @@ app.post('/chat', async (req, res) => {
     const model = process.env.OLLAMA_MODEL;
     const baseUrl = process.env.OLLAMA_API;
 
+    const messages = [
+        {
+            role: 'system',
+            content: '如果有需要，你可以调用 DirFiles 工具查找当前目录下的文件和文件夹'
+        },
+        { role: 'user', content: message }
+    ];
+
     try {
         const response = await axios.post(`${baseUrl}/api/chat`, {
             model,
-            messages: [
-                { 
-                    role: 'system', 
-                    content: '如果有需要，你可以调用 DirFiles 工具查找当前目录下的文件和文件夹' 
-                },
-                { role: 'user', content: message }
-            ],
+            messages,
             tools: [
                 {
                     type: 'function',
                     function: {
                         name: 'DirFiles',
                         description: '查找当前目录下的文件和文件夹',
-                        parameters: {
-                            type: 'object',
-                            properties: {
-                                directory: {
-                                    type: 'string',
-                                    description: '要查找的目录路径'
-                                }
-                            }
-                        }
                     }
                 }
             ],
             stream: false
         });
+        const reply = response.data.message.content;
 
-        const responseMessage = response.data.choices[0].message.content;
-        res.json({ response: responseMessage });
+        // console.log(response.data);
+
+        if (
+            response.data.message.tool_calls &&
+            response.data.message.tool_calls.length > 0 &&
+            response.data.message.tool_calls[0].function.name === 'DirFiles'
+        ) {
+            // 调用 DirFiles 工具
+            exec('dir', async (error, stdout, stderr) => {
+                if (error) {
+                    console.error('执行命令报错:', error);
+                    return;
+                }
+
+                if (stderr) {
+                    console.error('执行命令失败:', stderr);
+                    return;
+                }
+
+                // res.json({ reply: `当前目录下的文件和文件夹如下：\n${stdout}` });
+                messages.push({ role: 'tool', name: 'DirFiles', content: `当前目录下的文件和文件夹如下：\n${stdout}` });
+                
+                // 再次调用模型
+                const res = await axios.post(`${baseUrl}/api/chat`, {
+                    model,
+                    messages,
+                    stream: false
+                });
+                res.json({ reply: res.data.message.content });
+                
+                return;
+            });
+        } else {  // 不需要调用工具函数
+            res.json({ reply });
+        }
+
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
--- AI/functionCalling/demo/src/App.jsx
+++ AI/functionCalling/demo/src/App.jsx
@@ -1,121 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import React, { useState } from 'react'
 import './App.css'
+import { marked } from 'marked'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const [input, setInput] = useState('你好，帮我列出当前目录下的文件和文件夹');
+    const [think, setThink] = useState('');
+    const [result, setResult] = useState('');
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    const update = async () => {
+        if (!input) {
+            return;
+        }
+        const headers = {
+            'Content-Type': 'application/json',
+        }
+        
+        const response = await fetch('/api/chat', {
+            method: 'POST',
+            headers,
+            body: JSON.stringify({
+                messages: input
+            })
+        });
+        const data = await response.json();
+        console.log(data);
+        
+        setThink('');
+        setResult(marked(data.reply));
+    }
+
+    return (
+        <div className="container">
+            <div>
+                <label>输入：</label>
+                <input type="text" className="input" value={input} onChange={(e) => setInput(e.target.value)} />
+                <button className="submit" onClick={update}>提交</button>
+            </div>
+
+            <div className="output">
+                <div className="think">{think}</div>
+                <div className="result" dangerouslySetInnerHTML={{ __html: result }}></div>
+            </div>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    )
 }
-
-export default App
