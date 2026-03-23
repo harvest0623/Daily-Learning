d57f1b2193275fdcb19f2bcd98999d4785572639
harvest0623 <3367741939@qq.com>
2026-03-23 13:43:20 +0800

    面试-红绿灯算法

--- 面试考点/红绿灯算法/index.js
+++ 面试考点/红绿灯算法/index.js
@@ -19,14 +19,45 @@ function setColor(color, time) {
 // }
 // run();
 
+
 // 方法二：递归调用实现
-function run() {
-    setColor('红', 3000).then(() => {
-        setColor('绿', 2000).then(() => {
-            setColor('黄', 1000).then(() => {
-                run();
-            })
-        })
-    })
+// function run() {
+//     setColor('红', 3000).then(() => {
+//         setColor('绿', 2000).then(() => {
+//             setColor('黄', 1000).then(() => {
+//                 run();
+//             })
+//         })
+//     })
+// }
+// run();
+
+
+// 方法三：定义完整的灯序执行函数
+// function lightSequence() {
+//     return setColor('红', 3000)
+//         .then(() => setColor('绿', 2000))
+//         .then(() => setColor('黄', 1000));
+// }
+// // 按总时长（6秒）循环执行灯序
+// setInterval(lightSequence, 6000);
+// // 立即执行一次，避免首次等待6秒
+// lightSequence();
+
+
+// 方法四：生成器函数定义灯序
+function* lightGenerator() {
+    while(true) {
+        yield setColor('红', 3000);
+        yield setColor('绿', 2000);
+        yield setColor('黄', 1000);
+    }
 }
-run();
+// 自动执行生成器的函数
+async function runGenerator() {
+    const gen = lightGenerator();
+    for await (const step of gen) {
+        // 自动迭代执行每个灯的任务
+    }
+}
+runGenerator();
